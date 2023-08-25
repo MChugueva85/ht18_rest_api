3c95c924289fdd935767b180a51576c87ff4bd99
MChugueva85 <livingsmiling85@gmail.com>
2023-08-25 10:54:03 +0300

    added lombok model

--- src/test/java/in/reqres/tests/AuthApiTest.java
+++ src/test/java/in/reqres/tests/AuthApiTest.java
@@ -1,33 +1,41 @@
 package in.reqres.tests;
 
+import models.LoginBodyModel;
+import models.LoginResponseModel;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AuthApiTest extends TestBase{
+public class AuthApiTest extends TestBase {
     @Test
     @DisplayName("Успешная регистрация пользователя")
     @Tag("negative")
     void sucsessfulRegisterUserTest() {
-        String regData = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
 
-        given()
+        LoginBodyModel authData = new LoginBodyModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("pistol");
+
+        LoginResponseModel loginResponse = given()
                 .log().uri()
                 .log().method()
                 .log().body()
                 .contentType(JSON)
-                .body(regData)
+                .body(authData)
                 .when()
                 .post("/users")
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(201)
-                .body("email", is("eve.holt@reqres.in"))
-                .body("password", is("pistol"));
+                .extract().as(LoginResponseModel.class);
+        assertThat(loginResponse.getEmail()).contains("eve.holt@reqres.in");
+        assertThat(loginResponse.getPassword()).contains("pistol");
     }
 }
